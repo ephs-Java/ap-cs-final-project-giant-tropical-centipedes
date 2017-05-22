78baac1c89ae670c3475ac943a79b0856f90d637
Kyle George <kylgrg@gmail.com>
2017-05-22 12:28:55 -0500

    Kyle's next push

--- v1/Kyle/Collidable.java
+++ v1/Kyle/Collidable.java
@@ -1,9 +1,37 @@
 package Kyle;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
 public class Collidable {
+	int x, y;
 	
+	ArrayList<Collidable> otherColliders = new ArrayList<Collidable>();
 	
-	public Collidable(int x, int y, int scale) {
+	String type;
+	
+	int size;
+	
+	boolean visable = true;
+	
+	public Collidable(int x, int y, int scale, String type) {
+		this.x = x;
+		this.y = y;
 		
+		size = scale;
+		
+		this.type = type;
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.black);
+		g.drawRect(x, y, size, size);
+	}
+	
+	public void display(Graphics g) {
+		if(visable) {
+			draw(g);
+		}
 	}
 }
--- v1/Kyle/Map.java
+++ v1/Kyle/Map.java
@@ -1,10 +1,59 @@
 package Kyle;
 
+import java.awt.Graphics;
+import java.util.ArrayList;
+
 public class Map {
+	
+	String map;
 	
 	int scale;
 	
+	int w, h;
+	
+	ArrayList board = new ArrayList();
+	
 	public Map(String m, int scale) {
 		this.scale = scale;
+		map = m;
+		
+		makeMap();
+	}
+	
+	public void makeMap() {
+		int x = 0, y = 0;
+		
+		int mx = 0;
+		int my = 0;
+		
+		for(int i = 0; i < map.length(); i++) {
+			
+			
+			if(map.charAt(i) == '*') {
+				board.add(new Wall(x * scale, y * scale, scale));
+			}
+			
+			if(map.charAt(i) == '-') {
+				y++;
+				x = 0;
+			}
+			else {
+				x++;
+			}
+			
+			mx = x > mx ? x : mx;
+			my = y > my ? y : my;
+		}
+		
+		w = (mx) * scale + 1;
+		h = (my + 1) * scale + 23;
+	}
+	
+	int start = 0;
+	
+	public void draw(Graphics g) {
+		for(int i = 0; i < board.size(); i++) {
+			((Wall) board.get(i)).draw(g);
+		}
 	}
 }
