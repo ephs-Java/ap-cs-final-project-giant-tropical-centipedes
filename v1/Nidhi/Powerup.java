fd24942334e588f2f80ab309a18f0771aa55c7c8
90302704 <90302704@hs-06892-macair.ad.edenpr.org>
2017-05-23 13:41:58 -0500

    cherries!!

--- v1/Nidhi/Powerup.java
+++ v1/Nidhi/Powerup.java
@@ -2,10 +2,16 @@ package Nidhi;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import V1_Game.Vector;
 
 public class Powerup {
+	BufferedImage cherry;
 	public Vector pos = new Vector();
 	int size;
 	int scale;
@@ -14,14 +20,15 @@ public class Powerup {
 		pos = new Vector(x,y);
 		size = scale/2;
 		this.scale = scale;
+		try {
+			cherry = ImageIO.read(new File("cherries.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void draw(Graphics g) {
-		g.setColor(Color.MAGENTA);
-		int px = pos.x() + (scale - size) / 2;
-		int py = pos.y() + (scale - size) / 2;
-		g.fillOval(px, py, size, size);
-		g.setColor(Color.black);
-		g.drawOval(px, py, size, size);
+		g.drawImage(cherry, pos.x(),pos.y(), null);	
 	}
 }
